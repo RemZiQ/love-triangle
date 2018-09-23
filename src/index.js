19d62f62c7e203e974e48b2bcf5c6e5ee1dec8d2
RemZiQ <remziq1992@gmail.com>
2018-09-23 22:33:41 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    var count = 0;
+    for (let i = 1; i <= preferences.length; i++) {
+      if (preferences[i-1] <= preferences.length) {
+        var A = preferences[i-1];
+      }
+      if (preferences[A-1] <= preferences.length && preferences[A-1] != A) {
+        var B = preferences[A-1];
+      }
+      if (preferences[B-1] <= preferences.length && preferences[B-1] != B) {
+        var C = preferences[B-1];
+      }
+      if (C === i) {
+        count++;
+      }
+    }
+    return count/3;
+  };
